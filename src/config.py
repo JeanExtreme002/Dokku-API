de79b185a92aa6cf823d768f8371b1ecae560da5
JeanExtreme002 <jeangamerextreme@gmail.com>
2026-04-17 17:52:57 -0300

    test(api-tools): fix unit test of ResourceName

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-API_VERSION_NUMBER = "1.3.3"
+API_VERSION_NUMBER = "1.3.4"
 
 
 class DatabaseConfig:
--- src/tests/test_tools/test_name.py
+++ src/tests/test_tools/test_name.py
@@ -29,11 +29,11 @@ class TestResourceName(unittest.TestCase):
     def test_normalization_for_service(self):
         name = "My Resource!Name"
         rname = ResourceName(user=MockUser, name=name, resource_type=Service)
-        self.assertEqual(rname.normalized(), "my_resource_name")
-        self.assertEqual(str(rname), "my_resource_name")
-        self.assertEqual(rname.for_system(), "1_my_resource_name")
+        self.assertEqual(rname.normalized(), "my-resource-name")
+        self.assertEqual(str(rname), "my-resource-name")
+        self.assertEqual(rname.for_system(), "1-my-resource-name")
 
-    def test_normalization_from_app(self):
+    def test_decoding_app_name(self):
         name = "1-my-resource-name"
         rname = ResourceName(
             user=MockUser, name=name, resource_type=App, from_system=True
@@ -42,11 +42,11 @@ class TestResourceName(unittest.TestCase):
         self.assertEqual(str(rname), "my-resource-name")
         self.assertEqual(rname.for_system(), "1-my-resource-name")
 
-    def test_normalization_from_service(self):
-        name = "1_my_resource_name"
+    def test_decoding_service_name(self):
+        name = "1-my-resource-name"
         rname = ResourceName(
             user=MockUser, name=name, resource_type=Service, from_system=True
         )
-        self.assertEqual(rname.normalized(), "my_resource_name")
-        self.assertEqual(str(rname), "my_resource_name")
-        self.assertEqual(rname.for_system(), "1_my_resource_name")
+        self.assertEqual(rname.normalized(), "my-resource-name")
+        self.assertEqual(str(rname), "my-resource-name")
+        self.assertEqual(rname.for_system(), "1-my-resource-name")
